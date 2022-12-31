ee70748d4db290db23ede231df69ce0e8597d2de
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-31 21:22:59 +0200

    Add client handshake packet

--- src/packet/client.rs
+++ src/packet/client.rs
@@ -1,3 +1,5 @@
+use bytes::{Buf, BytesMut};
+
 use crate::logic::table::{Card, Suit};
 
 #[derive(Debug, Eq, PartialEq)]
@@ -24,6 +26,50 @@ impl ClientPacket {
             }),
             None => None,
         }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct ClientHandshakePacket {
+    pub num_names: u8,
+    pub names: Vec<String>,
+}
+
+impl ClientHandshakePacket {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        bytes.push(self.num_names);
+
+        for name in &self.names {
+            let name_bytes = name.as_bytes();
+            bytes.push(name_bytes.len() as u8);
+            bytes.extend_from_slice(name_bytes);
+        }
+
+        bytes
+    }
+
+    pub fn from_bytes(raw: &[u8]) -> Option<ClientHandshakePacket> {
+        if raw.len() < 1 {
+            return None;
+        }
+
+        let mut bytes = BytesMut::from(raw);
+        let num_names = bytes.get_u8();
+
+        let mut names = Vec::new();
+
+        for _ in 0..num_names {
+            let name_length = bytes.get_u8();
+            let mut name_buffer = vec![0; name_length as usize];
+            bytes.copy_to_slice(&mut name_buffer);
+            println!("{}", bytes.remaining());
+            let name = String::from_utf8_lossy(&name_buffer).to_string();
+            names.push(name);
+        }
+
+        Some(ClientHandshakePacket { num_names, names })
     }
 }
 
@@ -57,5 +103,37 @@ mod tests {
         };
 
         assert_eq!(packet_from_bytes, expected_packet);
+    }
+
+    #[test]
+    fn test_client_handshake_packet_to_bytes() {
+        let packet = ClientHandshakePacket {
+            num_names: 2,
+            names: vec![String::from("Alice"), String::from("Bob")],
+        };
+
+        let expected: Vec<u8> = vec![
+            0x02, 0x05, 0x41, 0x6C, 0x69, 0x63, 0x65, 0x03, 0x42, 0x6F, 0x62,
+        ];
+
+        let result = packet.to_bytes();
+
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_client_handshake_packet_from_bytes() {
+        let packet = ClientHandshakePacket {
+            num_names: 2,
+            names: vec![String::from("Alice"), String::from("Bob")],
+        };
+
+        let raw: Vec<u8> = vec![
+            0x02, 0x05, 0x41, 0x6C, 0x69, 0x63, 0x65, 0x03, 0x42, 0x6F, 0x62,
+        ];
+
+        let result = ClientHandshakePacket::from_bytes(&raw).unwrap();
+
+        assert_eq!(packet, result);
     }
 }
