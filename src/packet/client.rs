bec11a30b1187ea2f78f118e2d381c19ac9ee8ee
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-01-01 09:39:37 +0200

    Change ClientHandshakePacket to only have a name

--- src/packet/client.rs
+++ src/packet/client.rs
@@ -1,7 +1,5 @@
 use std::io::{Read, Result};
 
-use bytes::{Buf, BytesMut};
-
 use crate::logic::table::{Card, Suit};
 
 #[derive(Debug, Eq, PartialEq)]
@@ -33,67 +31,44 @@ impl ClientPacket {
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct ClientHandshakePacket {
-    pub num_names: u8,
-    pub names: Vec<String>,
+    name: String,
 }
 
 impl ClientHandshakePacket {
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
 
-        bytes.push(self.num_names);
-
-        for name in &self.names {
-            let name_bytes = name.as_bytes();
-            bytes.push(name_bytes.len() as u8);
-            bytes.extend_from_slice(name_bytes);
-        }
-
+        let name = &self.name;
+        let name_bytes = name.as_bytes();
+        bytes.push(name_bytes.len() as u8);
+        bytes.extend_from_slice(name_bytes);
         bytes
     }
 
-    pub fn from_bytes(raw: &[u8]) -> Option<ClientHandshakePacket> {
-        if raw.len() < 1 {
+    pub fn from_bytes(bytes: &[u8]) -> Option<String> {
+        if bytes.len() < 1 {
             return None;
         }
 
-        let mut bytes = BytesMut::from(raw);
-        let num_names = bytes.get_u8();
-
-        let mut names = Vec::new();
-
-        for _ in 0..num_names {
-            let name_length = bytes.get_u8();
-            let mut name_buffer = vec![0; name_length as usize];
-            bytes.copy_to_slice(&mut name_buffer);
-            println!("{}", bytes.remaining());
-            let name = String::from_utf8_lossy(&name_buffer).to_string();
-            names.push(name);
+        let name_length = bytes[0] as usize;
+        if bytes.len() < name_length + 1 {
+            return None;
         }
 
-        Some(ClientHandshakePacket { num_names, names })
+        let name = String::from_utf8_lossy(&bytes[1..name_length + 1].to_vec()).to_string();
+
+        Some(name)
     }
 
     pub fn get_client_handshake_packet<R: Read>(reader: &mut R) -> Result<ClientHandshakePacket> {
-        let mut num_names_buf = [0];
-        reader.read_exact(&mut num_names_buf)?;
-        let num_names = num_names_buf[0];
+        let mut name_length_buf = [0];
+        reader.read_exact(&mut name_length_buf)?;
+        let name_length = name_length_buf[0];
 
-        let mut names = Vec::new();
-
-        for _ in 0..num_names {
-            let mut name_length_buf = [0];
-            reader.read_exact(&mut name_length_buf)?;
-            let name_length = name_length_buf[0] as usize;
-
-            let mut name_buf = vec![0; name_length];
-            reader.read_exact(&mut name_buf)?;
-
-            let name = String::from_utf8_lossy(&name_buf).to_string();
-            names.push(name);
-        }
-
-        Ok(ClientHandshakePacket { num_names, names })
+        let mut name_buf = vec![0; name_length.into()];
+        reader.read_exact(&mut name_buf)?;
+        let name = String::from_utf8_lossy(&name_buf).to_string();
+        Ok(ClientHandshakePacket { name })
     }
 }
 
@@ -132,13 +107,10 @@ mod tests {
     #[test]
     fn test_client_handshake_packet_to_bytes() {
         let packet = ClientHandshakePacket {
-            num_names: 2,
-            names: vec![String::from("Alice"), String::from("Bob")],
+            name: String::from("Alice"),
         };
 
-        let expected: Vec<u8> = vec![
-            0x02, 0x05, 0x41, 0x6C, 0x69, 0x63, 0x65, 0x03, 0x42, 0x6F, 0x62,
-        ];
+        let expected: Vec<u8> = vec![0x05, 0x41, 0x6C, 0x69, 0x63, 0x65];
 
         let result = packet.to_bytes();
 
@@ -147,28 +119,22 @@ mod tests {
 
     #[test]
     fn test_client_handshake_packet_from_bytes() {
-        let packet = ClientHandshakePacket {
-            num_names: 2,
-            names: vec![String::from("Alice"), String::from("Bob")],
+        let expected = ClientHandshakePacket {
+            name: String::from("Alice"),
         };
 
-        let raw: Vec<u8> = vec![
-            0x02, 0x05, 0x41, 0x6C, 0x69, 0x63, 0x65, 0x03, 0x42, 0x6F, 0x62,
-        ];
+        let raw: Vec<u8> = vec![0x05, 0x41, 0x6C, 0x69, 0x63, 0x65];
 
         let result = ClientHandshakePacket::from_bytes(&raw).unwrap();
 
-        assert_eq!(packet, result);
+        assert_eq!(expected, ClientHandshakePacket { name: result });
     }
 
     #[test]
     fn test_get_client_handshake_packet() {
-        let data: Vec<u8> = vec![
-            0x02, 0x05, 0x41, 0x6C, 0x69, 0x63, 0x65, 0x03, 0x42, 0x6F, 0x62,
-        ];
+        let data: Vec<u8> = vec![0x05, 0x41, 0x6C, 0x69, 0x63, 0x65];
         let expected = ClientHandshakePacket {
-            num_names: 2,
-            names: vec![String::from("Alice"), String::from("Bob")],
+            name: String::from("Alice"),
         };
 
         let mut reader = &data[..];
